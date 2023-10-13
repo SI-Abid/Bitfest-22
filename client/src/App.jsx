d748218f2f17b6d2c4c94b26b5e19654a6c459c6
SI-Abid <siaabid5323@gmail.com>
2023-10-13 17:55:42 +0600

    create react app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
 
-export default function App() {
+export const App = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBhIqib-vFJwvMMPWlNTdKAAzc_CDorxOg",
+  });
+  // console.log(process.env);
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+
   return (
-    <h1 className='text-red-600'>App</h1>
-  )
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={10}
+        />
+      )}
+    </div>
+  );
 }
