c0c905c9210311254e34d2c3d9cba97bd5caf175
SI-Abid <siaabid5323@gmail.com>
2023-10-13 18:03:45 +0600

    api key removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,7 @@
+import React from 'react'
 
 export const App = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyBhIqib-vFJwvMMPWlNTdKAAzc_CDorxOg",
-  });
-  // console.log(process.env);
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
-
   return (
-    <>
-      <h1 className='text-red-600'>App</h1>
-      <button className="btn btn-primary">Primary</button>
-    </>
+    <h1 className='text-red-600'>App</h1>
   )
 }
